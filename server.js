7f80e89c341f3da4904890971d5ad082d628f4ff
Muhammadjon Murodullayev <Muhammadjonmurodullayev2005@gmail.com>
2025-11-30 16:35:33 +0500

    Render backend tayyor

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+require("dotenv").config();
+
+const videosRouter = require("./routes/videos.routes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-// test route
-app.get("/", (req, res) => {
-  res.send("✅ Beket Render.com da ishlayapti!");
-});
+// Lokalda video yuklash uchun "uploads" ochamiz
+app.use("/uploads", express.static("uploads"));
 
-// agar kerak bo'lsa backend route misoli
-app.get("/api/data", (req, res) => {
-  res.json([
-    { id: 1, name: "Render Backend", status: "Ishlayapti 🚀" },
-    { id: 2, name: "Free Tier", status: "Bepul va foydali 💸" },
-  ]);
-});
+// API
+app.use("/api/videos", videosRouter);
 
-// PORT
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server ishlayapti: ${PORT}`));
+app.listen(PORT, () => {
+  console.log("Server ishladi port:", PORT);
+});
